e7c1b1dfc08ecbccf5c430bc47d2e2e07e271292
Grzegorz Trukan <grzegorz.trukan@gmail.com>
2018-12-12 00:50:56 +0100

    check pings

--- garage.js
+++ garage.js
@@ -2,6 +2,16 @@ $(function() {
     var ip=0;
     for(var i=1;i<=254;i++) {
         ip="192.168.0."+i;
-        $("#info").append("<div>"+ip+"<div>");
+        checkIp(ip);
     }
 });
+
+function checkIp(ip) {
+    $.ajax({
+        url: "http://"+ip+"/ping",
+        timeout: 50,
+        success: function(result) {
+            console.log(ip);
+        }
+      })
+}
